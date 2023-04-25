d4ab81937beb3f3f0cf19f579bd2dca91744f4b9
Victoria Burton <vlaburton@gmail.com>
2023-04-25 14:07:26 +0100

    data set up and imports in homePage

---
+++ my-app/src/Components/Home Page/homePage.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import variablesQuizData from "../../Data/variablesQuizData";
+import arraysQuizData from "../../Data/arraysQuizData";
+import functionsQuizData from "../../Data/functionsQuizData";
+import Quiz from "../quizPage/quizPage";
+
+function HomePage() {
+  const [quizData, setQuizData] = useState(variablesQuizData);
+  const [quizName, setQuizName] = useState("Variables Quiz");
+  const handleQuizData = (e) => {
+    const quizName = e.target.value;
+    setQuizName(quizName);
+    if (quizName === "Variables Quiz") {
+      setQuizData(variablesQuizData);
+    } else if (quizName === "Arrays Quiz") {
+      setQuizData(arraysQuizData);
+    } else if (quizName === "Functions Quiz") {
+      setQuizData(functionsQuizData);
+    }
+  };
+
+  return (
+    <div>
+      {" "}
+      <h1>JavaScript Quiz</h1>
+      <button value="Variables Quiz" onClick={handleQuizData}>
+        {" "}
+      </button>
+      <button value="Arrays Quiz" onClick={handleQuizData}>
+        {" "}
+      </button>
+      <button value="Functions Quiz" onClick={handleQuizData}>
+        {" "}
+      </button>
+      <Quiz quizData={quizData} quizName={quizName} />
+    </div>
+  );
+}
---
+++ my-app/src/Components/quizPage/quizPage.js
@@ -0,0 +1,13 @@
+function Quiz() {
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [score, setScore] = useState(0);
+  
+    // Your logic to handle the quiz flow and rendering the quiz data based on the current question
+  
+    return (
+      // Your JSX code to render the quiz questions and options based on the quizData
+    );  
+  }
+  
+  export default Quiz;
+  
