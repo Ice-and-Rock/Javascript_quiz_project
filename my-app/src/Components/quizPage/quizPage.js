77a80ada06430b1e67617356dadb00078177fbce
Victoria Burton <vlaburton@gmail.com>
2023-04-26 11:18:38 +0100

    plan for quizPage

--- my-app/src/Components/quizPage/quizPage.js
+++ my-app/src/Components/quizPage/quizPage.js
@@ -1,5 +1,10 @@
 import React from "react";
-
+// This is where we pull the quiz data from the json file.
+// TO DO - add the 'next button' function
+// the next button will take us to the next question in the quiz data.
+// once all five questions have been answered, a results button will appear
+// once we click on the results button a different div will be rendered which shows the results of the quiz
+// MVP - display answer / alert correct or incorrect/show immediate result?
 const Quiz = ({ quizData, quizName }) => {
   return (
     <div>
