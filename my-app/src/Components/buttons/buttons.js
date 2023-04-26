5880e6b51bea959fc24e1b37ecb94ec8cb56ba7f
Victoria Burton <vlaburton@gmail.com>
2023-04-26 11:42:38 +0100

    trying to sort out button component

--- my-app/src/Components/buttons/buttons.js
+++ my-app/src/Components/buttons/buttons.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const VarQuizButton = () => {
-  const handleClick = () => {};
+//create a button to take the user to the variables quiz page
+//create a button to take the user to the arrays quiz page
+//create a button to take the user to the functions quiz page
+
+const VarQuizButton = ({ history }) => {
+  const handleClick = () => {
+    history.push("/variables-quiz"); // Change this to the correct path for the variables quiz page
+  };
+
+  return <button onClick={handleClick}>Go to Variables Quiz</button>;
 };
 
 export default VarQuizButton;
