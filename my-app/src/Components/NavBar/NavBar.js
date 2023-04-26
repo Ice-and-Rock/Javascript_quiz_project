2f18fc74850cb1901138dad5282b43a97eb1e6d6
Tom Burns <Tomulvlup@gmail.com>
2023-04-26 15:06:15 +0100

    more css

--- my-app/src/Components/NavBar/NavBar.js
+++ my-app/src/Components/NavBar/NavBar.js
@@ -3,16 +3,17 @@ import Logo from '../../images/V3_Logo.png';
 
 function NavBar() {
   return (
-    <div className="NavBar">
-      <button>Variables</button>
-      <button>Arrays</button>
-      <button>Functions</button>
-      <div className="logo"> 
+    <header>
+      <div className="NavBar">
+        <div>
+          <button>Variables</button>
+          <button>Arrays</button>
+          <button>Functions</button>
+        </div>
         <img id="logo" src={Logo} alt="logo" />
       </div>
-      
-    </div>
-  )
+    </header>
+  );
 }
 
 export default NavBar;
