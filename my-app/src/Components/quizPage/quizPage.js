a6118ef31a056fa31ed5065542e667779da2486c
Victoria Burton <vlaburton@gmail.com>
2023-04-27 11:43:37 +0100

    quizPage buttons update

--- my-app/src/Components/quizPage/quizPage.js
+++ my-app/src/Components/quizPage/quizPage.js
@@ -6,92 +6,120 @@
 // MVP - display answer / alert correct or incorrect/show immediate result?
 
 import React, { useState } from "react";
-export default function Quiz(){
-  
-
+export default function Quiz() {
   const arraysQuizData = [
-    
-      
-      {questionText: "How do you declare an empty array in JavaScript?",
+    {
+      questionText: "How do you declare an empty array in JavaScript?",
       optionText: [
-        {answerText: "let arr = []", isCorrect: true},
-        {answerText: "let arr ={}", isCorrect: false},
-        {answerText: "let arr = '' ", isCorrect: false},
-        {answerText: "let arr = new Array()", isCorrect: false}
-      ],
-    },
-    
-      
-      {questionText: "How do you access the third element of an array named 'myArray'?", 
-      optionText: [
-      {answerText: "myArray[3]", isCorrect: false},
-      {answerText: "myArray(2)", isCorrect: false},
-      {answerText: "myArray[2]", isCorrect: true},
-      {answerText: "myArray.2", isCorrect: false}
+        { answerText: "let arr = []", isCorrect: true },
+        { answerText: "let arr ={}", isCorrect: false },
+        { answerText: "let arr = '' ", isCorrect: false },
+        { answerText: "let arr = new Array()", isCorrect: false },
       ],
     },
 
-    {questionText: "Which method adds an element to the end of an array?",
-     optionText: [
-        {answerText: "push()", isCorrect: true},
-        {answerText: "pop()", isCorrect: false},
-        {answerText: "unshift()", isCorrect: false},
-        {answerText: "shift()", isCorrect: false}
+    {
+      questionText:
+        "How do you access the third element of an array named 'myArray'?",
+      optionText: [
+        { answerText: "myArray[3]", isCorrect: false },
+        { answerText: "myArray(2)", isCorrect: false },
+        { answerText: "myArray[2]", isCorrect: true },
+        { answerText: "myArray.2", isCorrect: false },
       ],
     },
 
-      {questiontext: "Which method removes the last element of an array and returns it?",
+    {
+      questionText: "Which method adds an element to the end of an array?",
       optionText: [
-        {answerText: "push()", isCorrect: false},
-        {answerText: "pop()", isCorrect: true},
-        {answerText: "unshift()", isCorrect: false},
-        {answerText: "shift()", isCorrect: false}
+        { answerText: "push()", isCorrect: true },
+        { answerText: "pop()", isCorrect: false },
+        { answerText: "unshift()", isCorrect: false },
+        { answerText: "shift()", isCorrect: false },
       ],
     },
 
-    {questionText: "Consider the following code. What will be the output?",
-      "codeSnippet": "const arr = [1, 2, 3, 4]; arr.splice(1, 2, 'a', 'b'); console.log(arr);",
+    {
+      questionText:
+        "Which method removes the last element of an array and returns it?",
       optionText: [
-        {answerText: "[1, 2, 3, 4]", isCorrect: false},
-        {answerText: "[1, 'a', 'b', 4]", isCorrect: true},
-        {answerText: "[1, 2, 'a', 'b']", isCorrect: false},
-        {answerText: "['a', 'b', 3, 4]", isCorrect: false} 
+        { answerText: "push()", isCorrect: false },
+        { answerText: "pop()", isCorrect: true },
+        { answerText: "unshift()", isCorrect: false },
+        { answerText: "shift()", isCorrect: false },
       ],
-    }
-  ]
+    },
 
+    {
+      questionText: "Consider the following code. What will be the output?",
+      codeSnippet:
+        "const arr = [1, 2, 3, 4]; arr.splice(1, 2, 'a', 'b'); console.log(arr);",
+      optionText: [
+        { answerText: "[1, 2, 3, 4]", isCorrect: false },
+        { answerText: "[1, 'a', 'b', 4]", isCorrect: true },
+        { answerText: "[1, 2, 'a', 'b']", isCorrect: false },
+        { answerText: "['a', 'b', 3, 4]", isCorrect: false },
+      ],
+    },
+  ];
 
   // console.log("number 2", quizData);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const handleAnswerButtonClick = (isCorrect) => {
+    if (isCorrect === true) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < arraysQuizData.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
 
   return (
     <div>
-      {/* <h2>{quizName}</h2> */}
-    
       <p>THIS IS A TEST</p>
-      <div className='app'>
-   {/* HINT: replace "false" with logic to display the 
+      <div className="app">
+        {/* HINT: replace "false" with logic to display the 
       score when the user has answered all the questions */}
-   {false ? (
-    <div className='score-section'>You scored 1 out of {arraysQuizData.length}</div>
-   ) : (
-    <>
-     <div className='question-section'>
-      <div className='question-count'>
-       <span>Question 1</span>/{arraysQuizData.length}
+        {false ? (
+          <div className="score-section">
+            {" "}
+            You Scored {score} out of {arraysQuizData.length}
+          </div>
+        ) : (
+          <>
+            <div className="question-section">
+              <div className="question-count">
+                <span>Question 1</span>/{arraysQuizData.length}
+              </div>
+              <div className="question-text">
+                {" "}
+                {arraysQuizData[currentQuestion].questionText}
+              </div>
+            </div>
+
+            <div className="answer-section">
+              {arraysQuizData[currentQuestion].optionText.map(
+                (answerOption) => (
+                  <button
+                    onClick={() =>
+                      handleAnswerButtonClick(answerOption.isCorrect)
+                    }
+                  >
+                    {}
+                  </button>
+                )
+              )}
+            </div>
+          </>
+        )}
       </div>
-      <div className='question-text'>This is where the question text should go</div>
-     </div>
-     <div className='answer-section'>
-      <button>Answer 1</button>
-      <button>Answer 2</button>
-      <button>Answer 3</button>
-      <button>Answer 4</button>
-     </div>
-    </>
-   )}
-  </div>
-</div>
+    </div>
   );
-};
-
-
+}
