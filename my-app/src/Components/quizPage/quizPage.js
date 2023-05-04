8b5b6b81c66b2bb1e4ddcfaabb1414b5f7a05505
nick v <nvalente.electrics@gmail.com>
2023-05-04 17:06:33 +0200

    added the different topics

--- my-app/src/Components/quizPage/quizPage.js
+++ my-app/src/Components/quizPage/quizPage.js
@@ -1,80 +1,49 @@
-// This is where we pull the quiz data from the json file.
-// TO DO - add the 'next button' function
-// the next button will take us to the next question in the quiz data.
-// once all five questions have been answered, a results button will appear
-// once we click on the results button a different div will be rendered which shows the results of the quiz
-// MVP - display answer / alert correct or incorrect/show immediate result?
+
+// TO DO ----------------
+// Each time an answer is selected the quiz uses the next quesiton in the array
+// once all five questions have been answered, a results 0/array.length will appear
+// once the results are rendered a new button appears to take the quiz again
+// MVP - display question / answers / results
 
 import React, { useState } from "react";
-// onclick not working 
-export default function Quiz({ quizName }) {
-  const arraysQuizData = [
-    {
-      questionText: "How do you declare an empty array in JavaScript?",
-      optionText: [
-        { answerText: "let arr = []", isCorrect: true },
-        { answerText: "let arr ={}", isCorrect: false },
-        { answerText: "let arr = '' ", isCorrect: false },
-        { answerText: "let arr = new Array()", isCorrect: false },
-      ],
-    },
 
-    {
-      questionText:
-        "How do you access the third element of an array named 'myArray'?",
-      optionText: [
-        { answerText: "myArray[3]", isCorrect: false },
-        { answerText: "myArray(2)", isCorrect: false },
-        { answerText: "myArray[2]", isCorrect: true },
-        { answerText: "myArray.2", isCorrect: false },
-      ],
-    },
+// FUNCTIONS --------------------------------
+ 
+// VARIABLES --------------------------------
+ 
+// ARRAYS --------------------------------
+  
 
-    {
-      questionText: "Which method adds an element to the end of an array?",
-      optionText: [
-        { answerText: "push()", isCorrect: true },
-        { answerText: "pop()", isCorrect: false },
-        { answerText: "unshift()", isCorrect: false },
-        { answerText: "shift()", isCorrect: false },
-      ],
-    },
+  // ------------------ Quiz Component ------------------
 
-    {
-      questionText:
-        "Which method removes the last element of an array and returns it?",
-      optionText: [
-        { answerText: "push()", isCorrect: false },
-        { answerText: "pop()", isCorrect: true },
-        { answerText: "unshift()", isCorrect: false },
-        { answerText: "shift()", isCorrect: false },
-      ],
-    },
+        // TO DO
+          // Change the quizName and quizData to be props ✅
+          // Use the handleClick from the App.js file to change the data ✅
+          // Make sure that the quiz works with all of the data ✅
 
-    {
-      questionText: "Consider the following code. What will be the output?",
-      codeSnippet:
-        "const arr = [1, 2, 3, 4]; arr.splice(1, 2, 'a', 'b'); console.log(arr);",
-      optionText: [
-        { answerText: "[1, 2, 3, 4]", isCorrect: false },
-        { answerText: "[1, 'a', 'b', 4]", isCorrect: true },
-        { answerText: "[1, 2, 'a', 'b']", isCorrect: false },
-        { answerText: "['a', 'b', 3, 4]", isCorrect: false },
-      ],
-    },
-  ];
 
+  export default function Quiz({ quizName, quizData }) {
+
+    // set up useStates for the elecments of the quiz ----------------------
+      // currentQuestion is the index of the array
+      // showScore is a boolean that is false by default
+      // score is the number of correct answers using boolean from Q data
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showScore, setShowScore] = useState(false);
   const [score, setScore] = useState(0);
 
+    // Adds one to the score if the answer is correct ----------------------      
+      // Includes the NEXT question when answer is clicked
   const handleAnswerButtonClick = (isCorrect) => {
     if (isCorrect === true) {
       setScore(score + 1);
     }
 
-    const nextQuestion = currentQuestion + 1;
-    if (nextQuestion < arraysQuizData.length) {
+      // Moves the quiz to the next question --------------------------------
+        // stops at the max length of the array
+        // if you're at the end of the array, show the score
+  const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < quizData.length) {
       setCurrentQuestion(nextQuestion);
     } else {
       setShowScore(true);
@@ -87,7 +56,7 @@ export default function Quiz({ quizName }) {
         {showScore ? (
           <div className="score-section">
             {" "}
-            You Scored {score} out of {arraysQuizData.length}
+            You Scored {score} out of {quizData.length}
             <button
               onClick={() => {
                 setShowScore(false);
@@ -103,28 +72,28 @@ export default function Quiz({ quizName }) {
             <div className="question-section">
               <div className="question-count">
                 <span>Question {currentQuestion + 1}</span>/
-                {arraysQuizData.length}
+                {quizData.length}
               </div>
               <h2 className="question-text"> {quizName}</h2>
               <div className="question-text">
                 {" "}
-                {arraysQuizData[currentQuestion].questionText}
+                {quizData[currentQuestion].questionText}
               </div>
               {/*
                 The section below I am conditionally rendering a component
 
-                if arraysQuizData[currentQuestion].codeSnippet is true/not false
+                if quizData[currentQuestion].codeSnippet is true/not false
 
-                then render this div and text  <div>{arraysQuizData[currentQuestion].codeSnippet}</div>
+                then render this div and text  <div>{quizData[currentQuestion].codeSnippet}</div>
               */}
 
-              {arraysQuizData[currentQuestion].codeSnippet && (
-                <div id="snippet" className="answer-section">{arraysQuizData[currentQuestion].codeSnippet}</div>
+              {quizData[currentQuestion].codeSnippet && (
+                <div id="snippet" className="answer-section">{quizData[currentQuestion].codeSnippet}</div>
               )}
             </div>
 
             <div className="answer-section">
-              {arraysQuizData[currentQuestion].optionText.map(
+              {quizData[currentQuestion].optionText.map(
                 (answerOption) => (
                   <button
                     onClick={() =>
